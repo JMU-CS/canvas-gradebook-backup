e5a96969c245e233f2fe81281a193068b749e34a
Michael Stewart <stewarmc@jmu.edu>
2022-12-16 16:02:29 -0500

    works for me 😂

---
+++ README.md
@@ -0,0 +1,6 @@
+Backup the scores for Canvas assignments (either 1 by its id, or all) to a CSV file that is importable into Canvas's gradebook.
+```
+usage: backup.py [-h] [--canvas_key CANVAS_KEY] [--canvas_url CANVAS_URL] [-o OUTFILE] [--assignment ASSIGNMENT]
+                 [--all]
+                 course
+```
---
+++ backup.py
@@ -0,0 +1,182 @@
+import argparse
+from canvasapi import Canvas
+import os
+import sys
+from datetime import datetime
+import csv
+
+"""
+Given a Canvas course id (and optionally a single assignment id), create a csv file that stores the current scores for all students for all assignments (that have the matching assignment id if it was provided). 
+This csv file should be formatted such that it could be imported into Canvas to restore the scores.
+"""
+
+
+canvas = None
+canvas_student_data = {}
+course_student_id_to_sis_id = {}
+sis_id_to_course_student_id = {}
+
+CANVAS_REQUIRED_FIELDS = [
+    "Student",
+    "ID",
+    "SIS User ID",
+    "SIS Login ID",
+    "Section",
+]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--canvas_key",
+        default=None,
+        help="your canvas account token. see: \
+            https://canvas.instructure.com/doc/api/file.oauth.html#manual-token-generation",
+    )
+
+    parser.add_argument(
+        "--canvas_url",
+        default=None,
+        help="the URL of your canvas instance, e.g. https://canvas.jmu.edu/",
+    )
+
+    parser.add_argument("course", help="canvas course id")
+
+    parser.add_argument("-o", "--outfile", help="file to write the backup to")
+
+    parser.add_argument("--assignment", help="canvas assignment id", required=False)
+
+    parser.add_argument(
+        "--all", action="store_true", help="backup the scores for all assignments"
+    )
+
+    # parser.add_argument(
+    #     "--wet_run",
+    #     action="store_true",
+    #     help="really assign the scores")
+
+    args = parser.parse_args()
+
+    # DRY = not args.wet_run
+
+    canvas_key = args.canvas_key
+    canvas_url = args.canvas_url
+    if canvas_key is None:
+        canvas_key = os.environ["CANVAS_KEY"]
+    if canvas_url is None:
+        canvas_url = os.environ["CANVAS_URL"]
+    if canvas_key is None or canvas_url is None:
+        print(
+            "must provide canvas api key and url via either the optional \
+            flags or the environment variables: CANVAS_KEY and CANVAS_URL"
+        )
+        sys.exit(1)
+
+    if args.assignment is None and not args.all:
+        print("must provide either an assignment id or the --all flag")
+        sys.exit(1)
+
+    # ambiguous intent
+    if args.all and args.assignment is not None:
+        print("cannot provide both an assignment id and the --all flag")
+        sys.exit(1)
+
+    global canvas
+    canvas = Canvas(canvas_url, canvas_key)
+    # try:
+    the_course = canvas.get_course(args.course)
+    course_students = [s for s in the_course.get_recent_students()]
+    global course_student_id_to_sis_id
+    course_student_id_to_sis_id = {s.id: s.sis_user_id for s in course_students}
+    global sis_id_to_course_student_id
+    sis_id_to_course_student_id = {s.sis_user_id: s.id for s in course_students}
+    global canvas_student_data
+    for section in the_course.get_sections():
+        for student in section.get_enrollments():
+            if student.type == "StudentEnrollment":
+                student_info = [
+                    student.user["sortable_name"],
+                    sis_id_to_course_student_id[student.sis_user_id],
+                    student.sis_user_id,
+                    student.user["login_id"],
+                    section.name,
+                ]
+                canvas_student_data[student.sis_user_id] = {
+                    CANVAS_REQUIRED_FIELDS[i]: student_info[i]
+                    for i in range(len(CANVAS_REQUIRED_FIELDS))
+                }
+    if args.assignment is not None:
+        the_assignment = the_course.get_assignment(args.assignment)
+        data = backup_single_assignment(the_course, the_assignment)
+        backup_path = filename(the_course, the_assignment, outfile=args.outfile)
+        write_backup(backup_path, data)
+    else:
+        # must be that args.all is True
+        data = backup_all_assignments(the_course)
+        merged = merge(data)
+        backup_path = filename(the_course, all=True, outfile=args.outfile)
+        write_backup(backup_path, merged)
+
+
+def backup_all_assignments(the_course):
+    global canvas
+    all_assns = [s for s in the_course.get_assignments()]
+    results = {}
+    # for i in range(5):
+    # a = all_assns[i]
+    for a in all_assns:
+        results[f"{a.name} ({a.id})"] = backup_single_assignment(the_course, a)
+    return results
+
+
+def merge(data):
+    merged = {}
+    for single_assn_scores in data.values():
+        for v in single_assn_scores:
+            if v["SIS User ID"] not in merged:
+                merged[v["SIS User ID"]] = {}
+            merged[v["SIS User ID"]] |= v
+    return merged
+
+
+def backup_single_assignment(the_course, the_assignment):
+    all = [s for s in the_assignment.get_submissions()]
+
+    subs = []
+    for s in all:
+        sis_id = course_student_id_to_sis_id[s.user_id]
+        if sis_id not in canvas_student_data:
+            print(f"{sis_id} not in {canvas_student_data.keys()}")
+            sys.exit(1)
+
+        score = ""
+        if not s.missing:
+            score = s.score
+        student_values = canvas_student_data[sis_id] | {
+            f"{the_assignment.name} ({the_assignment.id})": score
+        }
+        subs.append(student_values)
+    return subs
+
+
+def write_backup(filename, data):
+    fields = [v for v in data.values()][0].keys()
+
+    with open(filename, "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(data.values())
+
+
+def filename(course, assignment=None, all=None, outfile=None):
+    if outfile is not None:
+        return outfile
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    if all is not None:
+        return f"{timestamp}-{course.id}-ALL.bk.csv"
+    else:
+        return f"{timestamp}-{course.id}-{assignment.name.replace(' ', '_')}.bk.csv"
+
+
+if __name__ == "__main__":
+    main()
